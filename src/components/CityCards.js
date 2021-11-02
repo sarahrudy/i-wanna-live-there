e272474e713d6507001b081281a3849611b49532
Logan Vincent <ldvsimp@gmail.com>
2021-11-02 10:12:51 -0600

    Create CityCards component

---
+++ src/components/CityCards.js
@@ -0,0 +1,13 @@
+import React from "react";
+import CityCards from './CityCards';
+
+const CityCards = () => {
+
+  return(
+    <section>
+      
+    </section>
+  )
+};
+
+export default CityCards;
