b261923039b8686baaf94ff00f4da79ecc57bd31
Logan Vincent <ldvsimp@gmail.com>
2021-11-02 10:11:08 -0600

    Create CitiesContainer component

---
+++ src/components/CitiesContainer.js
@@ -0,0 +1,13 @@
+import React from "react";
+import CitiesContainer from './CitiesContainer';
+
+const CitiesContainer = () => {
+
+  return(
+    <section>
+      
+    </section>
+  )
+};
+
+export default CitiesContainer;
