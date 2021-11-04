a856d32b19c07b9f799d4c55e36d8bae671718a7
Logan Vincent <ldvsimp@gmail.com>
2021-11-03 20:09:21 -0600

    Add other files

---
+++ src/mockData.js
@@ -0,0 +1,9 @@
+// import { denver } from '../src/images/denver.png';
+
+export const mockData = {
+  "cities": [
+    {
+      "image": "https://image.tmdb.org/t/p/original//6CoRTJTmijhBLJTUNoVSUNxZMEI.jpg"
+    }
+  ]
+}
