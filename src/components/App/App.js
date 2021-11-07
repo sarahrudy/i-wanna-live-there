81d91b1fb15f912ad695b6d72dc28a570f68568c
Sarah <sarahrudy@gmail.com>
2021-11-07 13:17:22 -0700

    Add Router for different pages

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,16 +1,18 @@
 import React, { Component } from 'react';
+import { Route, Switch } from 'react-router-dom';
 import './App.css';
 import Nav from '../Nav/Nav';
 import Cities from '../Cities/Cities';
-import SearchBar from '../SearchBar/SearchBar'
-import CityDetails from '../CityDetails/CityDetails'
-import { getAllCities } from '../../apiCalls'
+import SearchBar from '../SearchBar/SearchBar';
+import CityDetails from '../CityDetails/CityDetails';
+import { getAllCities } from '../../apiCalls';
 
 class App extends Component {
   constructor() {
     super()
     this.state = {
       cities: [],
+      error: '',
     }
   }
 
@@ -20,18 +22,24 @@ class App extends Component {
       .catch(error => console.log(error))
   }
   
-
   render() {
+    // add error handling here, like if !this.state.error and there are no movies yet, show a loading message
     return (
       <main className='app'>
         <Nav />
-        <div className="main-container">
-        <SearchBar />
-        <Cities className="city-cards" cities={this.state.cities} />
-        <CityDetails details={this.state.cities} />
-        </div>
+        <Switch>
+          <Route exact path='/'>
+            <SearchBar />
+            <Cities cities={ this.state.cities } /> 
+          </Route>
+          <Route exact path='/:id' render={({ match }) => {
+            // const cityToRender = this.state.cities.find(city => city.id === parseInt(match.params.id))
+            return <CityDetails id={(match.params.id)} />
+          }} />
+        </Switch>
       </main>
-    );
+    )
   }
 }
+
 export default App;
