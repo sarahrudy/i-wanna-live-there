745d46d3c94dde90a95c30f5399e18e7cafacc97
Sarah <sarahrudy@gmail.com>
2021-11-07 23:12:50 -0700

    Add search icon to searchBar

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,11 +1,13 @@
 import React from "react";
 import './SearchBar.css';
+import searchIcon from '../../images/search.png'
 
 // this will probably need to be a class component since it will hold its own state
 const SearchBar = () => {
 
   return(
-    <div>
+    <div className='search-bar-container'>
+      <img src={ searchIcon } className='search-icon' alt='search button'></img>
       <input type='searchbar' className='search-bar' placeholder='Search cities here...' />
     </div>
   )
