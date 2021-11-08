00ab0ed9a486982944be16011c29ae14ddd771b1
Sarah <sarahrudy@gmail.com>
2021-11-08 09:46:23 -0700

    Add Link back to home if user clicks on title

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import './Nav.css';
+import { Link } from 'react-router-dom'
 
 const Nav = () => {
   return(
     <header className='nav-container'>
-      <h1 className="nav-h1">softWhere?</h1>
+      <Link to="/">
+        <h1 className="nav-h1">softWhere?</h1>
+      </Link>
       <p className="nav-p">TOP US CITIES TO LIVE AS A SOFTWARE DEVELOPER</p>
     </header>
   )
